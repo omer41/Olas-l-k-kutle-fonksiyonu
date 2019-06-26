93d71dc1431167660e7792f9b090d35ccc7ac1cb
Ömer Cızzak <omer.cizzak@gmail.com>
2019-06-26 16:34:22 +0300

    ilk commit

---
+++ ödev1_GuvercinYuvasi_Prensibi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print("Güvercin Yuvası Prensine uyan farlı bir örneği kodlayalım. ")
+print("8 kişinin toplandığı bir salonda, içlerinden en az 2 tanesinin doğum günü haftanın aynı gününe denk gelecektir. ")
+print("Bu soruyu güvercin yuvası prensibine atıfta bulunarak çözelim.")
+
+print("                     ----------------ÇÖZÜM-------------")
+print("Toplantıdaki kişi sayısı: 8")
+print("Haftanın günleri: 7")
+print(" Burada anahtar, obje sayısının (güvercinler), kişiler- özellik sayısından (yuvalar) günler, aylar vs.- en az bir fazla olmasıdır. Matematiksel tanımlamak gerekirse n+1 sayıda (veya daha fazla) objeniz varsa ve bunları n sınıfta kategorize etmek istiyorsanız en az iki objeniz aynı kategoriye ait olacaktır.")
+
+print("Kişi Sayısı/Haftanın Günleri => 8/7 en az iki kişi aynı günde doğmuş olacaktır. ")
+
+    
